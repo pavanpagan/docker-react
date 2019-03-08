5634a57e8c6a9e818037c9f5a44c39411686c21b
vrvwebinterns <vrvwebinterns@gmail.com>
2019-03-08 18:19:27 +0530

    latest

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+ 10>0;
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
 });
